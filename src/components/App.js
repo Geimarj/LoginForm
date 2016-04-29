318cfa0d0a451d2c2caf4d03c6214021d6fd60f7
geimarj <macalinaomargie25@gmail.com>
2016-04-29 17:24:24 +0800

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,64 @@
 import React from 'react';
+import {Well,
+    FormGroup,
+    ControlLabel,
+    FormControl,
+    HelpBlock,
+    Button,
+    ButtonGroup
+} from 'react-bootstrap'
 
-export default  class TodoApp extends React.Component{
+export default  class App extends React.Component{
  constructor(props){
      super(props);
  }
  
+ goToRegister(){
+     this.props.history.push("/register");
+ }
+ 
  render(){
+     const wellStyle={
+         width:400,
+         height:500,
+         marginLeft:'auto',
+         marginTop:'10px',
+         marginRight:'auto'
+     };
      return (
-         <div>
-         <h1>Welcome to ReactJS</h1>
+         <div classname="container">
+         <Well style={wellStyle}>
+         <legend>Please LogIn</legend>
+         <form>
+         <FormGroup>
+         <ControlLabel>Enter Username</ControlLabel>
+         <FormControl
+         type="text"
+         placeholder="Enter your username"
+         />
+         <FormControl.Feedback/>
+         <HelpBlock></HelpBlock>
+         </FormGroup>
+         
+         <FormGroup>
+         <ControlLabel>Enter Password</ControlLabel>
+         <FormControl
+         type="password"
+         placeholder="Enter your password"
+         />
+         <FormControl.Feedback/>
+         <HelpBlock></HelpBlock>
+         </FormGroup>
+         
+        <div className="btncontainer">
+        <ButtonGroup>
+         <Button bsStyle="primary" type="submit">Login</Button>
+          <Button bsStyle="info" type="button"
+          onClick={this.goToRegister.bind(this)}>Register</Button>
+          </ButtonGroup>
+          </div>
+         </form>
+         </Well>
          </div>
      );
  }
